d0bd8f7574add938614c59c398ebcebb5f5fb6be
Siwakon <tiger.prasang@gmail.com>
2022-06-26 16:36:07 +0700

    Update

--- portman_ws/src/turtlesim_control/launch/via_point_follower.launch.py
+++ portman_ws/src/turtlesim_control/launch/via_point_follower.launch.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
 
 def generate_launch_description():
     turtlesim = Node(
         package='turtlesim',
         executable='turtlesim_node',
+        parameters=[
+            {'background_b': 255},
+            {'background_g': 255},
+            {'background_r': 255},
+        ],
     )
     controller = Node(
         package='turtlesim_control',
         executable='controller.py',
+        parameters=[
+            {'gain': 10.0},
+            {'speed': 2.0},
+        ],
+        remappings=[
+            ('/pose','turtle1/pose'),
+            ('/cmd_vel','turtle1/cmd_vel'),
+        ],
     )
     scheduler = Node(
         package='turtlesim_control',
         executable='scheduler.py',
     )
+    spawn_turtle2 = ExecuteProcess(
+        cmd=[['ros2 service call /spawn turtlesim/srv/Spawn "{x: 5.0, y: 5.0, theta: 0.0, name: \'ttt5\'}"']],
+        shell=True,
+    )
 
-    entity_to_run = [turtlesim, controller, scheduler]
+
+    entity_to_run = [turtlesim, controller, scheduler, spawn_turtle2]
     return LaunchDescription(entity_to_run)
--- portman_ws/src/turtlesim_control/scripts/controller.py
+++ portman_ws/src/turtlesim_control/scripts/controller.py
@@ -12,16 +12,17 @@ from std_srvs.srv import Empty
 class Controller(Node):
     def __init__(self):
         super().__init__('controller')
-        self.command_publisher = self.create_publisher(Twist,'/turtle1/cmd_vel',10)
+        self.command_publisher = self.create_publisher(Twist,'/cmd_vel',10)
         timer_period = 0.1
         self.timer = self.create_timer(timer_period,self.timer_callback)
-        self.pose_subscription = self.create_subscription(Pose,'/turtle1/pose',self.pose_callback,10)
+        self.pose_subscription = self.create_subscription(Pose,'/pose',self.pose_callback,10)
         self.pose = Pose()
         self.set_goal_service = self.create_service(SetGoal,'/set_goal',self.set_goal_callback)
         self.enable_service = self.create_service(Empty,'/enable_goal',self.enable_callback)
         self.notival_arrival_client = self.create_client(Empty,'/notival_arrival')
         self.goal = np.array([2.0,3.0])
         self.set_isEnable = False
+        self.declare_parameters(namespace='',parameters=[('gain',5.0),('speed',2.0),])
 
     def timer_callback(self):
         if self.set_isEnable:
@@ -34,10 +35,10 @@ class Controller(Node):
         current_position = np.array([self.pose.x,self.pose.y])
         dp = self.goal-current_position
         e = np.arctan2(dp[1],dp[0])-self.pose.theta
-        K = 3.0
+        K = self.get_parameter('gain').get_parameter_value().double_value
         w = K*np.arctan2(np.sin(e),np.cos(e))
         if np.linalg.norm(dp)>0.1:
-            v = 2.0
+            v = self.get_parameter('speed').get_parameter_value().double_value
         else:
             v = 0.0
             w = 0.0
